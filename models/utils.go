00634eee42ace4883b2bc8898091924b63f46bdb
Andrei Mikhalenia <mikhalenia.a@gmail.com>
2024-05-28 23:15:46 +0100

    fix: change go generator

---
+++ models/utils.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+)
+
+// isNil checks if an input is nil
+func isNil(i interface{}) bool {
+	if i == nil {
+		return true
+	}
+	switch reflect.TypeOf(i).Kind() {
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.UnsafePointer, reflect.Interface, reflect.Slice:
+		return reflect.ValueOf(i).IsNil()
+	case reflect.Array:
+		return reflect.ValueOf(i).IsZero()
+	}
+	return false
+}
+
+// A wrapper for strict JSON decoding
+func newStrictDecoder(data []byte) *json.Decoder {
+	dec := json.NewDecoder(bytes.NewBuffer(data))
+	dec.DisallowUnknownFields()
+	return dec
+}
